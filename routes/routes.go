77a9b6f12e238c28720e3ee850d143ed9b07df05
Taswoor2507 <taswoorhossein72@gmail.com>
2025-01-14 17:10:31 +0500

    add logger middleware for routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	handler "golang_project_one/handlers"
+	loggerMiddleware "golang_project_one/middleware"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -12,7 +13,7 @@ func RegisterRoutes() *mux.Router {
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to the Golang Project"))
 	}).Methods("GET")
-	r.HandleFunc("/items", handler.GetAllItems).Methods("GET")
-	r.HandleFunc("/items/{id}", handler.GetItemById).Methods("GET")
+	r.Handle("/items", loggerMiddleware.LoggerMiddleware(http.HandlerFunc(handler.GetAllItems))).Methods("GET")
+	r.Handle("/items/{id}", loggerMiddleware.LoggerMiddleware(http.HandlerFunc(handler.GetItemById))).Methods("GET")
 	return r
 }
